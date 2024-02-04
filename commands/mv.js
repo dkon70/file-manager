a8affd6ca7aa219a73517b56344bacdd19068928
dkon70 <dkon70@inbox.ru>
2024-02-05 01:55:30 +0700

    feat: add mv error handling

--- commands/mv.js
+++ commands/mv.js
@@ -1,19 +1,33 @@
-import rm from "./rm.js";
 import path from 'path';
 import fs from 'fs';
 
 async function mv(args) {
   const [arg1, arg2] = args;
-  try {
-    const pathToInputFile = path.resolve(arg1);
-    const pathToOutputFile = path.resolve(arg2, path.basename(pathToInputFile));
-    const readStream = fs.createReadStream(pathToInputFile);
-    const writeStream = fs.createWriteStream(pathToOutputFile);
-    readStream.pipe(writeStream);
-    await rm(pathToInputFile);
-  } catch (err) {
-    console.error("Error: Can't move file");
-  }
+  const pathToInputFile = path.resolve(arg1);
+  const pathToOutputFile = path.resolve(arg2, path.basename(pathToInputFile));
+  const readStream = fs.createReadStream(pathToInputFile);
+  const writeStream = fs.createWriteStream(pathToOutputFile);
+
+  readStream.on('error', function(err) {
+    if (err.message.startsWith('ENOENT')) {
+      console.error("Error: No such file");
+    } else {
+      console.error("Error: Can't move file");
+    }
+  });
+
+  writeStream.on('error', function(err) {
+    if (err.message.startsWith('ENOENT')) {
+      console.error("Error: Can't move file. Destination directory is incorrect.");
+    } else if (err.message.startsWith('EPERM')) {
+      console.error("Error: Can't move file. Permission denied.");
+    }
+  });
+
+  readStream.pipe(writeStream);
+  fs.rm(pathToInputFile, function() {
+
+  });
 }
 
 export default mv;
