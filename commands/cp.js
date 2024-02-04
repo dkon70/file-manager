f2844c7dc8f30be3789e436b93f278d8c2624b7d
dkon70 <dkon70@inbox.ru>
2024-02-05 01:58:03 +0700

    feat: add cp error handling

--- commands/cp.js
+++ commands/cp.js
@@ -3,15 +3,28 @@ import fs from 'fs';
 
 async function cp(args) {
   const [arg1, arg2] = args;
-  try {
-    const pathToInputFile = path.resolve(arg1);
-    const pathToOutputFile = path.resolve(arg2, path.basename(pathToInputFile));
-    const readStream = fs.createReadStream(pathToInputFile);
-    const writeStream = fs.createWriteStream(pathToOutputFile);
-    readStream.pipe(writeStream);
-  } catch (err) {
-    console.error("Error: Can't copy file");
-  }
+  const pathToInputFile = path.resolve(arg1);
+  const pathToOutputFile = path.resolve(arg2, path.basename(pathToInputFile));
+  const readStream = fs.createReadStream(pathToInputFile);
+  const writeStream = fs.createWriteStream(pathToOutputFile);
+
+  readStream.on('error', function(err) {
+    if (err.message.startsWith('ENOENT')) {
+      console.error("Error: No such file");
+    } else {
+      console.error("Error: Can't move file");
+    }
+  });
+
+  writeStream.on('error', function(err) {
+    if (err.message.startsWith('ENOENT')) {
+      console.error("Error: Can't move file. Destination directory is incorrect.");
+    } else if (err.message.startsWith('EPERM')) {
+      console.error("Error: Can't move file. Permission denied.");
+    }
+  });
+
+  readStream.pipe(writeStream);
 }
 
 export default cp;
