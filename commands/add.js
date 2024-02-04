ce55041eee35f0c41d126beb41cc29b9a0df6f72
dkon70 <dkon70@inbox.ru>
2024-02-04 19:33:35 +0700

    fix: implement close file descriptor after file create

--- commands/add.js
+++ commands/add.js
@@ -4,10 +4,11 @@ import path from 'path';
 async function add(...file) {
   const fileName = file.join(' ').replace(/'|"/g, '');
   const pathToFile = path.join(process.cwd(), fileName);
-  fs.open(pathToFile, 'wx', function(err) {
+  fs.open(pathToFile, 'wx', function(err, fd) {
     if (err) {
       err.code.startsWith('EPERM') ? console.log("Error: Permission denied.") : console.error("Error: Can't create file.");
     }
+    fs.close(fd);
   });
 }
 
